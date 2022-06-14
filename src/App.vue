043ff7abc48dd6bbc9b533e2eec18a91c5322da0
sichan75 <is528629@gmail.com>
2022-06-15 00:55:21 +0900

    feat: 기초적인 TodoItem 컴포넌트 구현

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app__wrapper">
+    <div>
+      <div v-for="item in items" v-bind:key="item">
+        <TodoItem :title="item" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "./components/TodoItem.vue";
+
+const items = () => ["아이템1", "아이템2"];
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodoItem,
+  },
+  data() {
+    return {
+      items: items(),
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app__wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrapper">
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style>
+.wrapper {
+  max-width: 500px;
+  width: 100%;
+  padding: 5px 12px;
+  border: 1px solid black;
+}
+p {
+  margin: 0;
+}
+</style>
